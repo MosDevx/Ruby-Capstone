5c9d2d89e3617a723e9bf2e877cdf6a660d46d01
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-24 22:36:37 +0630

    add list_options method in main.rb

---
+++ main.rb
@@ -0,0 +1,13 @@
+def list_options
+    "Welcome to my catalog of things
+    1 - List of all books
+    2 - List of all music albums
+    3 - List of games
+    4 - List of all generes
+    5 - List of all labels
+    6 - List of all authors
+    7 - Add a book
+    8 - Add a music album
+    9 - Add a game
+    10 - Exit"
+end
