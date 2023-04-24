712e5b043eaf03350ffe110a7e5ac6c8bdf91b64
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-24 19:37:22 +0300

    Add `can_be_archived` method

--- Classes/item.rb
+++ Classes/item.rb
@@ -1,21 +1,28 @@
 require 'securerandom'
+require 'date'
+
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date
 
-  def initialize(publish_date: '')
+  def initialize(publish_date: '', genre: '', label: '', source: '', author: '')
     @publish_date = publish_date
-		generate_id
+    generate_id
+    @genre = genre
+    @label = label
+    @source = source
+    @author = author
   end
 
-  def call_me()
-    puts 'hey'
+  def generate_id
+    @id = SecureRandom.uuid.delete('-')[0, 8]
   end
 
-	def generate_id
-		@id = SecureRandom.uuid.delete('-')[0,8]
-	end
+  def can_be_archived?
+    last_decade = Date.today - (10 * 365)
+    @published_date < last_decade
+  end
+
   private
-
 
   attr_reader :id, :archived
 end
