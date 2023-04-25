84e685a37f6a7559bd85b4b924a68af4c3950284
MosDevx <mosesk.wamae@gmail.com>
2023-04-25 17:05:16 +0300

    refactor book constructor to recieve an options hash

--- Classes/book.rb
+++ Classes/book.rb
@@ -1,19 +1,20 @@
 require_relative 'item'
 
 class Book < Item
-	attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-	def initialize(publish_date: '', genre: '', label: '', source: '', author: '', publisher: '', cover_state: '')
-		@publisher = publisher
-		@cover_state = cover_state
-		super(publish_date: publish_date, genre: genre, label: label, source: source, author: author)
-	end
+  def initialize(publisher: '', cover_state: '', item_init: {})
+    @publisher = publisher
+    @cover_state = cover_state
+    super(**item_init)
+  end
 
-	def can_be_archived?
-		super() || @cover_state == 'bad'
-	end
+  def can_be_archived?
+    super() || @cover_state == 'bad'
+  end
 end
 
-bk = Book.new(publish_date: '2020-10-10', genre: 'Horror', label: 'The Shining', source: 'Library', author: 'Stephen King', publisher: 'Penguin', cover_state: 'bad')
-
-puts bk.can_be_archived?
+bk = Book.new(publisher: 'OReilly', cover_state: 'good',
+              item_init: { publish_date: '2019-01-01', genre: 'IT', label: 'Ruby',
+                           source: 'Internet', author: 'John Doe' })
+puts bk.publish_date
