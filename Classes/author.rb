77b136eaaa46b376467ff47370788f34d212a8e2
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-25 14:19:53 +0300

    Add `game` and `author` properties

--- Classes/author.rb
+++ Classes/author.rb
@@ -1,1 +1,14 @@
-class Author
+require 'securerandom'
+
+class Author 
+    def initialize(first_name: '', last_name: '')
+        @first_name = first_name
+        @last_name = last_name
+        generate_id
+        @items = []
+    end
+
+    def generate_id
+        @id = SecureRandom.uuid.delete('-')[0, 8]
+    end
+end
--- Classes/game.rb
+++ Classes/game.rb
@@ -1,9 +1,7 @@
-require 'securerandom'
-
 class Game < Item
 
-    def initialize(multiplayer: false, last_played_at: '')
-        super(publish_date: publish_date, genre: genre, label: label, source: source, author: author)
+    def initialize(multiplayer: false, last_played_at:)
+        super(id: publish_date: publish_date, genre: genre, label: label, source: source, author: author)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
     end
