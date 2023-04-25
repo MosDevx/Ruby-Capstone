ebd4b716d94b406fe56c15c1663e2753133ecec4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-25 14:43:54 +0300

    Fix `game` and `author` methods

--- Classes/author.rb
+++ Classes/author.rb
@@ -1,20 +1,21 @@
 require 'securerandom'
 
-class Author 
-    attr_reader :item
-    def initialize(first_name: '', last_name: '')
-        @first_name = first_name
-        @last_name = last_name
-        generate_id
-        @items = []
-    end
+class Author
+  attr_reader :item
 
-    def generate_id
-        @id = SecureRandom.uuid.delete('-')[0, 8]
-    end
+  def initialize(first_name: '', last_name: '')
+    @first_name = first_name
+    @last_name = last_name
+    generate_id
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.author = self
-    end
+  def generate_id
+    @id = SecureRandom.uuid.delete('-')[0, 8]
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
 end
--- Classes/game.rb
+++ Classes/game.rb
@@ -1,23 +1,19 @@
 class Game < Item
-    attr_reader :author
-    attr_accessor :multiplayer, :last_played_at
+  attr_reader :author
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize(multiplayer: false, last_played_at:)
-        super(id: publish_date: publish_date, genre: genre, label: label, source: source, author: author)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(last_played_at:, multiplayer: false, **args)
+    super(**args)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def can_be_archived?
-        if Date.parse(@last_played_at) < Date.today.prev_year(2) && item.can_be_archived?
-            true
-        else
-            false
-        end
-    end
+  def can_be_archived?
+    Date.parse(@last_played_at) < Date.today.prev_year(2) && super
+  end
 
-    def @author=(author)
-        @author = author
-        author.item << self unless author.item.include?(self)
-    end
+  def author=(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
 end
