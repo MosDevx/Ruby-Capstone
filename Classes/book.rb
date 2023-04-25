8acbf5ce10d37020431a577e7f799a9d0f75b4c1
MosDevx <mosesk.wamae@gmail.com>
2023-04-25 16:23:30 +0300

    implement class book properties

--- Classes/book.rb
+++ Classes/book.rb
@@ -1,8 +1,11 @@
 require_relative 'item'
 
 class Book < Item
+	attr_accessor :publisher, :cover_state
 
-	def initialize(publish_date: '', genre: '', label: '', source: '', author: '')
+	def initialize(publish_date: '', genre: '', label: '', source: '', author: '', publisher: '', cover_state: '')
+		@publisher = publisher
+		@cover_state = cover_state
 		super(publish_date: publish_date, genre: genre, label: label, source: source, author: author)
 	end
 
