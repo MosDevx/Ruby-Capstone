ed7ea0ce0518db5c7e5c7d295851bac27441b6df
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-25 21:18:58 +0630

    fix rubocop errors

--- Classes/music_album.rb
+++ Classes/music_album.rb
@@ -1,15 +1,15 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
+  attr_accessor :on_spotify
 
-    def initialize (on_spotify: false, publish_date:, name:)
-        super(publish_date)
-        @on_spotify = on_spotify
-        @name = name
-    end
+  def initialize(publish_date:, name:, on_spotify: false)
+    super(publish_date)
+    @on_spotify = on_spotify
+    @name = name
+  end
 
-    def can_be_archived?
-        super && @on_spotify
-    end
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,21 +1,21 @@
 require_relative 'spec_helper'
 
 describe 'MusicAlbum' do
-    before(:each) do
-        @album = MusicAlbum.new(on_spotify: true, publish_date: '2023-04-25', name: 'Lover')
+  before(:each) do
+    @album = MusicAlbum.new(on_spotify: true, publish_date: '2023-04-25', name: 'Lover')
+  end
+
+  context 'testing MusicAlbum class' do
+    it 'create the instance of MusicAlbum class' do
+      expect(@album).to be_an_instance_of(MusicAlbum)
     end
 
-    context 'testing MusicAlbum class' do
-        it 'create the instance of MusicAlbum class' do
-            expect(@album).to be_an_instance_of(MusicAlbum)
-        end
-
-        it 'should return the correct date' do
-            expect(@album.publish_date).to eql('2023-04-25')
-        end
-
-        it 'should return correct spotify status' do
-            expect(@album.on_spotify).to eql(true)
-        end
+    it 'should return the correct date' do
+      expect(@album.publish_date).to eql('2023-04-25')
     end
+
+    it 'should return correct spotify status' do
+      expect(@album.on_spotify).to eql(true)
+    end
+  end
 end
