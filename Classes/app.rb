e7d26117293def7d0c89c5683996c5fda8ef88b7
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-25 20:24:08 +0630

    implement initialize method in App class

--- Classes/app.rb
+++ Classes/app.rb
@@ -1,7 +1,19 @@
 require_relative './music_album'
 require_relative './genre'
+require_relative './game'
+require_relative './book'
+require_relative './label'
+require_relative './author'
 
 class App
-    attr_reader :albums, :genres
+    attr_accessor :albums, :genres, :books, :games, :labels, :authors
 
+    def initialize
+        @albums = []
+        @genres = []
+        @books = []
+        @games = []
+        @labels = []
+        @authors = []
+    end
 end
