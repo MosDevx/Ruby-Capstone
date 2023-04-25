d48fb451e241146a896374d50f371bc65270fd9e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-25 14:34:03 +0300

    Add `game` and `author` methods

--- Classes/author.rb
+++ Classes/author.rb
@@ -1,6 +1,7 @@
 require 'securerandom'
 
 class Author 
+    attr_reader :item
     def initialize(first_name: '', last_name: '')
         @first_name = first_name
         @last_name = last_name
@@ -10,5 +11,10 @@ class Author
 
     def generate_id
         @id = SecureRandom.uuid.delete('-')[0, 8]
+    end
+
+    def add_item(item)
+        @items << item
+        item.author = self
     end
 end
--- Classes/game.rb
+++ Classes/game.rb
@@ -1,8 +1,23 @@
 class Game < Item
+    attr_reader :author
+    attr_accessor :multiplayer, :last_played_at
 
     def initialize(multiplayer: false, last_played_at:)
         super(id: publish_date: publish_date, genre: genre, label: label, source: source, author: author)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
+    end
+
+    def can_be_archived?
+        if Date.parse(@last_played_at) < Date.today.prev_year(2) && item.can_be_archived?
+            true
+        else
+            false
+        end
+    end
+
+    def @author=(author)
+        @author = author
+        author.item << self unless author.item.include?(self)
     end
 end
