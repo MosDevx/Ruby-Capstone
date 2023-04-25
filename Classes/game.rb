12e2ad025acd48d632e52b474256d63376e9f8d4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-25 14:05:11 +0300

    Add `game` and `author` classes

---
+++ Classes/game.rb
@@ -0,0 +1,10 @@
+require 'securerandom'
+
+class Game < Item
+
+    def initialize(multiplayer: false, last_played_at: '')
+        super(publish_date: publish_date, genre: genre, label: label, source: source, author: author)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+end
