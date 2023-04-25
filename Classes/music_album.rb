937814277f84431de9587c1dbe554b86ac4c19c2
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-25 17:32:28 +0630

    implement initialize method in MusicAlbum class

--- Classes/music_album.rb
+++ Classes/music_album.rb
@@ -1,4 +1,10 @@
 require './item'
 
 class MusicAlbum < Item
+    attr_accessor :on_spotify
+
+    def initialize (on_spotify: false, publish_date, genre)
+        super(publish_date, genre)
+        @on_spotify = on_spotify
+    end
 end
