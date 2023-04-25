3ab4c51dec475efcb3b0407357f8706b54a8af45
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-25 21:18:58 +0630

    fix rubocop errors

--- Classes/app.rb
+++ Classes/app.rb
@@ -6,47 +6,49 @@ require_relative './label'
 require_relative './author'
 
 class App
-    attr_accessor :albums, :genres, :books, :games, :labels, :authors
+  attr_accessor :albums, :genres, :books, :games, :labels, :authors
 
-    def initialize
-        @albums = []
-        @genres = []
-        @books = []
-        @games = []
-        @labels = []
-        @authors = []
+  def initialize
+    @albums = []
+    @genres = []
+    @books = []
+    @games = []
+    @labels = []
+    @authors = []
+  end
+
+  def add_music_album
+    puts 'Album title: '
+    title = gets.chomp.to_s
+    puts 'Publish data: '
+    date = gets.chomp.to_s
+    puts 'Genre: '
+    genre_name = gets.chomp.to_s
+    puts 'Is on spotify? [Y/N]: '
+    answer = gets.chomp
+    on_spotify = true if %w[Y y].include?(answer)
+    on_spotify = false if %w[N n].include?(answer)
+    puts 'New Music Album created! '
+
+    @albums << MusicAlbum.new(on_spotify, date, title)
+    @genres << Genre.new(genre_name)
+  end
+
+  def list_music_albums
+    if @albums.empty?
+      puts 'Please a music album'
+    else
+      @albums.each do |album|
+        puts "Title: \"#{album.name}\", Publish_date: #{album.publish_date}, Is on spotify?: #{album.on_spotify}"
+      end
     end
+  end
 
-    def add_music_album
-        puts 'Album title: '
-        title = gets.chomp.to_s
-        puts 'Publish data: '
-        date = gets.chomp.to_s
-        puts 'Genre: '
-        genre_name = gets.chomp.to_s
-        puts 'Is on spotify? [Y/N]: '
-        answer = gets.chomp
-        on_spotify = true if %w[Y y].include?(answer)
-        on_spotify = false if %w[N n].include?(answer)
-        puts 'New Music Album created! '
-
-        @albums << MusicAlbum.new(on_spotify, date, title)
-        @genres << Genre.new(genre_name)
+  def list_genres
+    if @genres.empty?
+      puts 'Please a music album'
+    else
+      @genres.each { |genre| puts "Genre: \"#{genre.name}\"" }
     end
-
-    def list_music_albums
-        if @albums.empty?
-            puts 'Please a music album'
-        else 
-            @albums.each { |album| puts "Title: \"#{album.name}\", Publish_date: #{album.publish_date}, Is on spotify?: #{album.on_spotify}" }
-        end
-    end
-
-    def list_genres
-        if @genres.empty?
-            puts 'Please a music album'
-        else 
-            @genres.each { |genre| puts "Genre: \"#{genre.name}\"" }
-        end
-    end
+  end
 end
