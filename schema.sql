63956d8008e6688021a8cd9df843839f2ac37b10
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-26 17:46:02 +0630

    create table for genre in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,13 @@
 CREATE TABLE music_album (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(250),
     on_spotify BOOLEAN,
     publish_date DATE,
     genre_id INT,
     FOREIGN KEY(genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE genre (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250)
 );
