da281ca8eb83b968f136c4c64209c7ece434aa5b
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-26 23:13:01 +0630

    update music_album table in schema.sql

--- schema.sql
+++ schema.sql
@@ -3,6 +3,10 @@ CREATE TABLE music_album (
     name VARCHAR(250),
     on_spotify BOOLEAN,
     publish_date DATE,
+    genre VARCHAR(250),
+    label VARCHAR(250),
+    source VARCHAR(250),
+    author VARCHAR(250),
     genre_id INT,
     FOREIGN KEY(genre_id) REFERENCES genre(id)
 );
