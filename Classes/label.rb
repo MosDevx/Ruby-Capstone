2ef7b3dfc0201824bd9d320e5db55c6cea2b0ef3
MosDevx <mosesk.wamae@gmail.com>
2023-04-26 05:21:59 +0300

    Revert "edit book.rb and item.rb to make can_be_archived  private"
    This reverts commit 9c3aa6d00123ecad982ce32ce52f04533d4b9014.
    
    Wrong commit message

--- Classes/label.rb
+++ Classes/label.rb
@@ -2,34 +2,29 @@ require 'securerandom'
 require_relative 'book'
 
 class Label
+  attr_accessor :title, :color
 
-	attr_accessor :title, :color
-	def initialize(title:'' ,color:'' )
-		@title = title
-		@color = color
-		@items = []
-		generate_id
-	end
+  def initialize(title: '', color: '')
+    @title = title
+    @color = color
+    @items = []
+    generate_id
+  end
 
+  def to_s
+    "Title:#{@title} Color:#{@color}"
+  end
 
+  def add_item(item)
+    item.label = self
+    @items << item
+  end
 
-	def to_s
-		"Title:#{@title} Color:#{@color}"
-	end
+  private
 
-	def add_item(item)
-		item.label = self
-		@items << item
-	end
+  attr_accessor :id, :items
 
-	private
-	attr_accessor :id, :items
-
-	def generate_id
-		@id = SecureRandom.uuid.delete('-')[0, 8]
-	end
+  def generate_id
+    @id = SecureRandom.uuid.delete('-')[0, 8]
+  end
 end
-
-lb = Label.new(title:'test', color:'red')
-lb.add_item(Book.new(publisher:'test', cover_state:'test', item_init:{publish_date:'2010-10-10', genre:'test', label:lb, source:'test', author:'test'}))
-puts lb.items[0].label
