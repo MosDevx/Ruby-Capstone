26910d42165442d1c5941cc2cb919e6c60b9e205
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-26 15:54:20 +0630

    add new test for Genre method in genre_spec.rb

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -9,5 +9,10 @@ describe 'genre' do
     it 'create the instance of Genre class' do
       expect(@genre).to be_an_instance_of(Genre)
     end
+    it 'should return the length of items' do
+      album = MusicAlbum.new(name: 'Lover', on_spotify: true, publish_date: '2023-04-25')
+      @genre.add_item(album)
+      expect(@genre.items.length).to eql 1
+    end
   end
 end
