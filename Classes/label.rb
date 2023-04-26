2d409e3ac59f01086327b548da67aeeecb13a3c6
MosDevx <mosesk.wamae@gmail.com>
2023-04-26 04:59:19 +0300

    implement class Label.rb

---
+++ Classes/label.rb
@@ -0,0 +1,26 @@
+require 'securerandom'
+
+class Label
+
+	attr_accessor :title, :color
+	def initialize(title:'' ,color:'' )
+		@title = title
+		@color = color
+		@items = []
+	end
+
+	def generate_id
+		@id = SecureRandom.uuid.delete('-')[0, 8]
+	end
+
+	def to_s
+		"#{@title} (#{@color})"
+	end
+
+	def add_item(item:item)
+		@items << Item
+	end
+
+	private
+	attr_accessor :id, :items
+end
