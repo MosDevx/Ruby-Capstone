a5a194e7ef54581f643c70feebf1a6ab41f6b402
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-26 15:41:40 +0630

    update app.rb

--- Classes/app.rb
+++ Classes/app.rb
@@ -1,20 +1,12 @@
 require_relative './music_album'
 require_relative './genre'
-require_relative './game'
-require_relative './book'
-require_relative './label'
-require_relative './author'
 
 class App
-  attr_accessor :albums, :genres, :books, :games, :labels, :authors
+  attr_accessor :albums, :genres
 
   def initialize
     @albums = []
     @genres = []
-    @books = []
-    @games = []
-    @labels = []
-    @authors = []
   end
 
   def add_music_album
