e12b957a05307d89ca43890c97f890ab13a7210d
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-26 15:46:38 +0630

    add add_genre method in MusicAlbum class

--- Classes/music_album.rb
+++ Classes/music_album.rb
@@ -3,10 +3,15 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date:, name:, on_spotify: false)
-    super(publish_date)
+  def initialize(name:, on_spotify: false, **args)
+    super(**args)
     @on_spotify = on_spotify
     @name = name
+  end
+
+  def add_genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
   end
 
   def can_be_archived?
