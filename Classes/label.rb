9c3aa6d00123ecad982ce32ce52f04533d4b9014
MosDevx <mosesk.wamae@gmail.com>
2023-04-26 05:21:13 +0300

    edit book.rb and item.rb to make can_be_archived  private

--- Classes/label.rb
+++ Classes/label.rb
@@ -2,29 +2,34 @@ require 'securerandom'
 require_relative 'book'
 
 class Label
-  attr_accessor :title, :color
 
-  def initialize(title: '', color: '')
-    @title = title
-    @color = color
-    @items = []
-    generate_id
-  end
+	attr_accessor :title, :color
+	def initialize(title:'' ,color:'' )
+		@title = title
+		@color = color
+		@items = []
+		generate_id
+	end
 
-  def to_s
-    "Title:#{@title} Color:#{@color}"
-  end
 
-  def add_item(item)
-    item.label = self
-    @items << item
-  end
 
-  private
+	def to_s
+		"Title:#{@title} Color:#{@color}"
+	end
 
-  attr_accessor :id, :items
+	def add_item(item)
+		item.label = self
+		@items << item
+	end
 
-  def generate_id
-    @id = SecureRandom.uuid.delete('-')[0, 8]
-  end
+	private
+	attr_accessor :id, :items
+
+	def generate_id
+		@id = SecureRandom.uuid.delete('-')[0, 8]
+	end
 end
+
+lb = Label.new(title:'test', color:'red')
+lb.add_item(Book.new(publisher:'test', cover_state:'test', item_init:{publish_date:'2010-10-10', genre:'test', label:lb, source:'test', author:'test'}))
+puts lb.items[0].label
