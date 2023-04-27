80c5ccd981106461fdb6be18560aa5820add1035
MosDevx <mosesk.wamae@gmail.com>
2023-04-27 09:39:49 +0300

    implement author_controller

--- Classes/app.rb
+++ Classes/app.rb
@@ -3,21 +3,18 @@ require_relative './genre'
 require 'json'
 
 class App
-  attr_accessor :albums, :genres,
+  attr_accessor :albums, :genres
 
   def initialize
     @albums = []
     @genres = []
     @label_controller = LabelController.new
     @author_controller = AuthorController.new
-   
-  end
-  
-  def list_books
   end
 
-  def add_book
-  end
+  def list_books; end
+
+  def add_book; end
 
   def add_music_album
     puts 'Album title: '
