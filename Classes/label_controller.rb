2b3e912788184876075db53773d4f7ffb4598496
MosDevx <mosesk.wamae@gmail.com>
2023-04-28 09:52:27 +0300

    Implement menu options and data storage for books

---
+++ Classes/label_controller.rb
@@ -0,0 +1,32 @@
+require_relative 'label'
+require_relative 'input_validator'
+
+class LabelController
+  include InputValidator
+  attr_accessor :labels
+
+  def initialize
+    @labels = []
+  end
+
+  def create_label
+    puts 'Please enter the following (Label) info: '
+    print 'Title: '
+    title = gets.chomp.to_s
+    print 'Color: '
+    color = fetch_valid_name('Color: ')
+    label = Label.new(title: title, color: color)
+    @labels.push(label)
+    puts 'New Label created!'
+  end
+
+  def list_labels
+    if @labels.empty?
+      puts 'Please create a label'
+    else
+      @labels.each do |label|
+        puts "Title: #{label.title}, Color: #{label.color}"
+      end
+    end
+  end
+end
