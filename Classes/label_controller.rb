a3e236fec39f292da8a29d0451a68e01c9e43b42
MosDevx <mosesk.wamae@gmail.com>
2023-04-28 20:05:20 +0300

    fix linters

--- Classes/label_controller.rb
+++ Classes/label_controller.rb
@@ -18,24 +18,23 @@ class LabelController
     print 'Color: '
     color = fetch_valid_name('Color: ')
 
-    full_name = title+color
- 
-      if new_label?(full_name)
-        label = Label.new(title: title, color: color)
-        @labels << label
-        puts
-        puts '+++ New Label created! +++'
-        puts
-        label
-      else
-        puts
-        puts 'Label already exists!'
-        existing_label = @labels.find {	|lbl| lbl.to_s == full_name.downcase }
-        puts 'Returning existing label...'
-        puts
-        existing_label
-      end
+    full_name = title + color
 
+    if new_label?(full_name)
+      label = Label.new(title: title, color: color)
+      @labels << label
+      puts
+      puts '+++ New Label created! +++'
+      puts
+      label
+    else
+      puts
+      puts 'Label already exists!'
+      existing_label = @labels.find {	|lbl| lbl.to_s == full_name.downcase }
+      puts 'Returning existing label...'
+      puts
+      existing_label
+    end
 
     # label = Label.new(title: title, color: color)
     # @labels.push(label)
