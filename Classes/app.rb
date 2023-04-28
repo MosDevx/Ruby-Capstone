4f2a8fc455fade6cf9f17b181988a784629996a9
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-28 15:03:48 +0630

    update list_music_albums and list_genre methods in app.rb

--- Classes/app.rb
+++ Classes/app.rb
@@ -66,7 +66,7 @@ class App
       puts 'Please a music album'
     else
       @albums.each do |album|
-        puts "Title: \"#{album.name}\", Publish_date: #{album.publish_date}, Is on spotify?: #{album.on_spotify}"
+        puts "Title: #{album["Title"]}, Publish_date: #{album["Publish_date"]}, Is on spotify?: #{album["Is on spotify?"]}"
       end
     end
   end
@@ -75,7 +75,7 @@ class App
     if @genres.empty?
       puts 'Please a music album'
     else
-      @genres.each { |genre| puts "Genre: \"#{genre.name}\"" }
+      @genres.each { |genre| puts "Genre: \"#{genre["Genre"]}\"" }
     end
   end
 
